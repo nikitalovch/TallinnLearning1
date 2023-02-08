1d3c6e9fd602ad58d1224d7bea78b82625122975
Nikita Lovtsikov <nikitalovtsikov@gmail.com>
2023-02-08 12:05:04 +0200

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Black");
-        System.out.println("White");
-        System.out.println("Red");
+        int a = 4;
+        int b = 5;
+        int c = a * b;
+
+        int z = 95;
+        double x = 1.8;
+        double y = 1.8 * 1.8;
+        double p = z / y;
+
+        System.out.println( a );
+        System.out.println( b );
+        System.out.println( c );
+        System.out.println( "Square root is: 20 c" );
+        System.out.println( z );
+        System.out.println( x );
+        System.out.println( y );
+        System.out.println( p );
     }
 }
